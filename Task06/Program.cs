b4098021d919e9f675a0a8c386e5c74b078de16d
Mariia <mariia.gusakova.msc@gmail.com>
2023-09-23 21:48:59 +0300

    Задача 4

---
+++ Task06/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine($"Случайное число: {number}");
+int numberA = number % 2;
+
+if (numberA == 0)
+{
+    Console.WriteLine($"{number} - четное число");
+}
+else
+{
+   Console.WriteLine($"{number} - нечетное число"); 
+}
